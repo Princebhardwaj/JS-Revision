57d52233b0cc6cadfa029ceceefcac3aa391cff4
Princebhardwaj <51474947+Princebhardwaj@users.noreply.github.com>
2025-11-20 07:21:35 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all code as newer versoin
+
+// alert(3 + 3); we are using nodejs not browser
+
+// documentation on tc39 and mdn
+
+console.log("prince");
+
+let name = "Prince"
+let age = 20
+let isLoggedIn = true
+
+// number => 2 to the power 53
+// big int
+// String => ""
+// boolean => true/false
+// null => standalone value : representation of empty value
+// undefined => variable declared but value not defined.
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
